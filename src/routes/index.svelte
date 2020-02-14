9f498309b4c268d289a937af95549b11727ee27f
ajboni <baj@mailbox.org>
2020-02-14 10:09:06 -0300

    back to preload

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,16 @@
+<script context="module">
+  import { posts, tags } from "../store.js";
+  export async function preload(page, session) {
+    const res = await this.fetch("index.json");
+    const json = await res.json();
+    posts.set(json.contents);
+    tags.set(json.tags);
+    return tags;
+  }
+</script>
+
 <script>
-  import { onMount } from "svelte";
-  import { posts, fetchPosts, tagFilter } from "../store.js";
+  import { fetchPosts, tagFilter } from "../store.js";
   import PostList from "./_postList.svelte";
   import Spinner from "./_spinner.svelte";
 </script>
@@ -9,10 +19,10 @@
   <title>Blog</title>
 </svelte:head>
 
-{#await fetchPosts($tagFilter)}
-  <Spinner caption="Loading posts..." />
-{:then value}
-  <PostList />
-{:catch error}
-  <p>Something went wrong: {error.message}</p>
-{/await}
+<!-- {#await fetchPosts($tagFilter)} -->
+<!-- <Spinner caption="Loading posts..." /> -->
+<!-- {:then value} -->
+<PostList />
+<!-- {:catch error} -->
+<!-- <p>Something went wrong: {error.message}</p> -->
+<!-- {/await} -->
--- src/store.js
+++ src/store.js
@@ -1,15 +1,11 @@
 import { writable, derived } from "svelte/store";
 export let posts = writable([]);
 export let tags = writable([]);
-export let tagFilter = writable({})
+export let tagFilter = writable({});
 
 export async function fetchPosts(params) {
-	const url = new URL("/index.json", window.location.href);
-	url.search = new URLSearchParams(params).toString();
+  //   const url = new URL("/index.json", window.location.href);
+  //   url.search = new URLSearchParams(params).toString();
 
-	const res = await fetch(url);
-	const json = await res.json();
-	posts.set(json.contents);
-	tags.set(json.tags)
+  return json;
 }
-
